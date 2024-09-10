ebf1f1842b5a5df5157aeeef94faadb12d3d48d9
RayraCMota <rayracmota@hotmail.com>
2024-09-09 21:01:30 -0300

    commit TypeScript

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(valor1, valor2) {
+    valor1 = 2;
+    valor2 = 5;
+    return valor1 * valor2;
+}
+function dizOi(nome) {
+    nome = 'Rayra';
+    return "Ola + `${nome}`";
+}
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+function multiplicacao(valor1: number, valor2: number) : number {
+
+    return valor1 * valor2;
+}
+
+console.log(multiplicacao(2, 5))
+
+
+function dizOi(nome: string) : string{
+
+    return "Ola " + `${nome}`
+}
+
+console.log(dizOi('Rayra'))
+
